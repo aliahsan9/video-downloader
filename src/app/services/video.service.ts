833020d3f367ce2484482b5a4adc46c27e7651bf
Ali Ahsan <aliahsan8751@gmail.com>
2026-02-09 18:51:41 +0500

    added backend path

--- src/app/services/video.service.ts
+++ src/app/services/video.service.ts
@@ -12,7 +12,7 @@ export interface VideoInfo {
   providedIn: 'root'
 })
 export class VideoService {
-  private apiUrl = 'https://video-downloader-bmfmbrbxf5dgezf3.centralindia-01.azurewebsites.net/api/video'; // your .NET API URL
+  private apiUrl = 'https://video-downloader-bmfmbrbxf5dgezf3.centralindia-01.azurewebsites.net/api'; // your .NET API URL
 
   constructor(private http: HttpClient) { }
 
